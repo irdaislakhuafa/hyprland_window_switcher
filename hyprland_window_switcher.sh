f34961dc155bcfecae93de2bbd4477393dbaf393
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-05-16 19:29:12 +0700

    user address instead of pid

--- hyprland_window_switcher.sh
+++ hyprland_window_switcher.sh
@@ -3,41 +3,41 @@ SEPARATOR=">"
 FORMAT="json"
 
 CLIENT_JSON_FIELDS="{
-	pid: .pid,
+	address: .address,
 	title: .title,
 	class: .class
 }";
 
-results=`hyprctl clients -j | jq -c ".[] | $CLIENT_JSON_FIELDS"`
+results=`hyprctl clients -j | jq -c ".[]"`
 windows=()
 
 while IFS= read -r row; do
 	# get some fields
-	pid=`echo "$row" | jq -r '.pid'`
+	address=`echo "$row" | jq -r '.address'`
 	title=`echo "$row" | jq -r '.title'`
 	class=`echo "$row" | jq -r '.class'`
-
+ 
 	if [ "$pid" = "-1" ]; then
 		continue
 	fi
-
-	windows+=( "$class $SEPARATOR $title $SEPARATOR $pid" )
+ 
+	windows+=( "$(printf "%s\t| %s \t %s" "$address" "$class" "$title")" )
 done <<< "$results"
 
 windows=$(printf "%s\n" "${windows[@]}")
-window=`echo "$windows" | fzf`
+window=`echo "$windows" | rofi -dmenu -matching normal -i`
 if [ "$window" = "" ]; then
 	echo "$NOTHING_DO_MESSAGE"
 	exit 0
 fi
 
-pid=`echo $window | cut -d "$SEPARATOR" -f 3 | cut -d " " -f 2`
-echo $pid
-if [ "$pid" = "" ]; then
+address=`echo $window | cut -d "|" -f 1`
+echo $address
+if [ "$address" = "" ]; then
 	echo "$NOTHING_DO_MESSAGE"
 	exit 0
 else
-	hyprctl dispatch focuswindow pid:$pid
+	hyprctl dispatch focuswindow address:$address
 fi
 
 
--- init.sh
+++ init.sh
@@ -5,4 +5,4 @@ DIR=`dirname "$(readlink -f "$0")"`
 alacritty \
 	-T "$APP_TITLE" \
 	-o font.size=$APP_FONT_SIZE \
-	-e zsh -c "$DIR/window_switcher.sh";
+	-e zsh -c "$DIR/hyprland_window_switcher.sh";
