6f2a109292b78e9e992390abb58cc2bd4649ff31
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-05-16 21:08:46 +0700

    remove unused codes

--- init.sh
+++
@@ -1,8 +0,0 @@
-APP_TITLE="Window Switcher";
-APP_FONT_SIZE=12;
-DIR=`dirname "$(readlink -f "$0")"`
-
-alacritty \
-	-T "$APP_TITLE" \
-	-o font.size=$APP_FONT_SIZE \
-	-e zsh -c "$DIR/hyprland_window_switcher.sh";
