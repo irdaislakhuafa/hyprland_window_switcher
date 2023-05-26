5ecefda436bd74c610eef6729a62f998cca719ed
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-05-26 23:46:37 +0700

    update submodules

--- hyprland_window_switcher.sh
+++ hyprland_window_switcher.sh
@@ -1,14 +1,14 @@
 NOTHING_DO_MESSAGE="Nothing do."
 
-format=`printf "\"\(.address) | \(.title) >> \(.class)\""`
+format=$(printf "\"\(.address) | \(.title) >> \(.class)\"")
 windows="$(hyprctl clients -j | jq -r ".[] | $format")"
-window=`echo "$windows" | rofi -dmenu -matching normal -i`
+window=$(echo "$windows" | rofi -dmenu -matching normal -i)
 if [ "$window" = "" ]; then
 	echo "$NOTHING_DO_MESSAGE"
 	exit 0
 fi
 
-address=`echo $window | cut -d "|" -f 1`
+address=$(echo $window | cut -d "|" -f 1)
 echo $address
 if [ "$address" = "" ]; then
 	echo "$NOTHING_DO_MESSAGE"
@@ -16,4 +16,3 @@ if [ "$address" = "" ]; then
 else
 	hyprctl dispatch focuswindow address:$address
 fi
-
